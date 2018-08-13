7537c7f35982cb5551484bac1e5fba9e16913ade
alsator <alsator@gmail.com>
2018-08-13 22:29:32 +0300

    using jquery input ip address control plugin

--- js/ip.js
+++ js/ip.js
@@ -1,6 +1,6 @@
 (function ($) {
 	$(document).ready(function () {
-		$('#ipv4').ipAddress();
-		$('#ipv6').ipAddress({v:6});
+		$('.ipv4').ipAddress();
+		$('.ipv6').ipAddress({v:6});
 	})
 })(jQuery);
